f66bf3b3b69401a1368d31646c41e3a0a9fc778e
Leo Chen <leo@pocketsquare.co.nz>
2024-05-31 16:36:02 +1200

    feat: updated the example code

--- example/example1/frontend/admin/src/router/index.js
+++ example/example1/frontend/admin/src/router/index.js
@@ -1,15 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import IndexVue from '../views/Index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: IndexVue
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {path: '/', name: 'home', component: IndexVue},
+        {path: '/:pathMatch(.*)*', name: 'not-found', component: IndexVue},
+    ]
 })
 
 export default router
--- example/example1/frontend/admin/src/stores/index.js
+++ example/example1/frontend/admin/src/stores/index.js
@@ -1,20 +1,23 @@
 import {defineStore} from "pinia"
 import isString from "lodash/isString"
 import isUndefined from "lodash/isUndefined"
+import Swal from "sweetalert2"
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: "bottom",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+    }
+})
 
 export const useDefault = defineStore("default", {
     state: () => ({
-        app_bridge: window?.shopify || null,
-        success_toast: {
-            active: false,
-            content: 'Success',
-            duration: 3000,
-        },
-        error_toast: {
-            active: false,
-            content: 'Error',
-            duration: 5000,
-        },
+        app_bridge: window?.shopify ?? null,
         // Add your internal endpoints here
         internal_endpoints: [
             {name: 'check', url: '/admin/check'},
@@ -22,14 +25,36 @@ export const useDefault = defineStore("default", {
         ],
     }),
     actions: {
-
+        showToast(message, payload = {}) {
+            payload = {
+                ...{
+                    isError: false,
+                    title: null,
+                    duration: 3000
+                }, ...payload
+            }
+            if (!this.checkAppBridge()) {
+                Toast.fire({
+                    icon: payload.isError ? "error" : 'success',
+                    text: message,
+                    timer: payload.duration,
+                })
+            } else {
+                this.app_bridge.toast.show(message, {
+                    duration: payload.duration,
+                    isError: payload.isError
+                })
+            }
+        },
         checkAppBridge() {
             if (this.app_bridge === null) {
-                this.error_toast.content = 'App bridge is not available'
-                this.error_toast.active = true
-                return false
+                Toast.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'App bridge is not available'
+                })
             }
-            return true
+            return this.app_bridge !== null
         },
         redirectAdmin(resource, target = '_top') {
             if (!this.checkAppBridge()) return
@@ -57,10 +82,24 @@ export const useDefault = defineStore("default", {
         errorCallback(err, isSaving) {
             console.error(err)
             if (isSaving) setTimeout(() => isSaving.value = false, 1000)
-            err = err?.data || err
-            this.error_toast.active = true
-            this.error_toast.content = err?.message ? err.message : 'Oops... Something went wrong'
-        }
+            err = err?.data ?? err
+            this.showToast(err?.message ? err.message : 'Oops... Something went wrong', {
+                isError: true,
+                title: 'Error',
+                duration: 5000
+            })
+        },
+        formValidation(schema, data, payload = null) {
+            try {
+                let values = schema.validateSync(data, payload ?? {abortEarly: false, stripUnknown: true})
+                return {rs: true, values, error: {}}
+            } catch (err) {
+                console.error('Validation fail', err.errors)
+                let error = {}
+                err.inner.forEach(v => error[v.path] = v.errors[0])
+                return {rs: false, values: {}, error}
+            }
+        },
     },
     getters: {
         getApi(state) {
