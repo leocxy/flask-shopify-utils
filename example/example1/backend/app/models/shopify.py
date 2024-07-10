f7f1bd2d2d8b8036992c0cb8f5fde86ea52fadbf
Leo Chen <leo@pocketsquare.co.nz>
2024-07-11 08:46:06 +1200

    chore: added the discount code example to the example1

--- example/example1/backend/app/models/shopify.py
+++ example/example1/backend/app/models/shopify.py
@@ -7,7 +7,7 @@
 # @Date    : 6/10/23 12:06 pm
 
 Here is an example how to inject function to Store model, since python 3.12, we should do this in another way
-"""
+
 from flask_shopify_utils.model import Store
 from json import loads, dumps
 
@@ -20,3 +20,101 @@ def set_extra(self, data: dict) -> None:
 
 Store.get_extra = lambda self: loads(self.extra) if self.extra else {}
 Store.set_extra = set_extra
+"""
+from flask_shopify_utils.model import BasicMethod, current_time
+from simplejson import loads, dumps
+# custom models
+from app import db
+
+
+class DiscountCode(db.Model, BasicMethod):
+    __tablename__ = 'discount_codes'
+    __table_args__ = (
+        db.PrimaryKeyConstraint('id'),
+        db.Index('store_did', 'store_id', 'code_id')
+    )
+    id = db.Column(db.Integer)
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    code_id = db.Column(db.BigInteger)
+    code_stamp = db.Column(db.SmallInteger, comment='0: Product, 1: Order, 2: Shipping')
+    code_type = db.Column(db.SmallInteger, default=0, comment="0: Manually, 1: Automatic")
+    code_name = db.Column(db.String(255), comment='Code / Title')
+    discount_method = db.Column(db.SmallInteger, default=0, comment='0: Fixed, 1: Percentage')
+    discount_value = db.Column(db.Integer, comment='Cent / Percentage')
+    requirement_type = db.Column(db.String(32))
+    requirement_value = db.Column(db.Integer, comment='Cent / Quantity')
+    customer_eligibility = db.Column(db.String(32))
+    one_use_per_customer = db.Column(db.SmallInteger, default=0)
+    limit_usage = db.Column(db.SmallInteger, default=0)
+    maximum_usage = db.Column(db.SmallInteger)
+    combine_with_product = db.Column(db.SmallInteger, default=0)
+    combine_with_order = db.Column(db.SmallInteger, default=0)
+    combine_with_shipping = db.Column(db.SmallInteger, default=0)
+    start_date = db.Column(db.DateTime, comment='UTC')
+    end_date = db.Column(db.DateTime, comment='UTC')
+    message = db.Column(db.String(255))
+    extra = db.Column(db.Text, comment='JSON')
+    created_at = db.Column(db.DateTime, default=current_time)
+    updated_at = db.Column(db.DateTime, default=current_time, onupdate=current_time)
+
+    @classmethod
+    def convert_code_type(cls, val, revert: bool = False):
+        if revert:
+            val = int(val)
+            return 'code' if val == 0 else 'auto'
+        return 0 if val == 'code' else 1
+
+    @classmethod
+    def convert_discount_method(cls, val, revert: bool = False):
+        if revert:
+            val = int(val)
+            return 'fixed' if val == 0 else 'percentage'
+        return 0 if val == 'fixed' else 1
+
+    def get_extra(self) -> dict:
+        return {} if self.extra is None else loads(self.extra)
+
+    def set_extra(self, data: dict) -> dict:
+        extra_data = self.get_extra()
+        extra_data.update(data)
+        self.extra = dumps(extra_data)
+        return extra_data
+
+    def to_dict(self) -> dict:
+        extra = self.get_extra()
+        output = dict(
+            id=self.code_id,
+            type=self.code_type,
+            code=None,
+            title=None,
+            discount_method=self.discount_method,
+            discount_value=self.discount_value,
+            requirement_type=self.requirement_type,
+            require_amount=None,
+            require_quantity=None,
+            customer_eligibility=self.customer_eligibility,
+            one_use_per_customer=self.one_use_per_customer == 1,
+            limit_usage=self.limit_usage == 1,
+            maximum_usage=self.maximum_usage,
+            combine_with_product=self.combine_with_product == 1,
+            combine_with_order=self.combine_with_order == 1,
+            combine_with_shipping=self.combine_with_shipping == 1,
+            start_date=self.start_date.strftime('%Y-%m-%dT%H:%M:%S') if self.start_date else None,
+            enable_end_date=False,
+        )
+        output.update(**extra)
+        if self.code_type == 0:
+            output['code'] = self.code_name
+        else:
+            output['title'] = self.code_name
+        if self.end_date:
+            output['enable_end_date'] = True
+            output['end_date'] = self.end_date.strftime('%Y-%m-%dT%H:%M:%S')
+        if self.requirement_type == 'total_amount':
+            output['require_amount'] = self.requirement_value
+        elif self.requirement_type == 'quantity':
+            output['require_quantity'] = self.requirement_value
+        output['type'] = self.convert_code_type(self.code_type, True)
+        if self.discount_method is not None:
+            output['discount_method'] = self.convert_discount_method(self.discount_method, True)
+        return output
--- example/example1/backend/app/utils/base.py
+++ example/example1/backend/app/utils/base.py
@@ -6,7 +6,7 @@
 # @Author  : Leo Chen<leo.cxy88@gmail.com>
 # @Date    : 6/10/23 4:22 pm
 """
-from os import path
+from os import path, getenv
 from logging import Formatter, Logger
 from logging.handlers import RotatingFileHandler
 from functools import wraps
@@ -15,8 +15,11 @@ from simplejson import dumps
 from sgqlc.operation import Operation
 from flask_shopify_utils.utils import GraphQLClient
 from flask_shopify_utils.model import Store
+from typing import Tuple, Optional
+from abc import abstractmethod, ABC
 # custom modules
-from app import app
+from app import app, db
+from app.models.shopify import DiscountCode
 from app.schemas.shopify import shopify as shopify_schema
 
 
@@ -115,3 +118,245 @@ def fn_debug(func):
         return result
 
     return decorator
+
+
+class DiscountHelper(ABC, BasicHelper):
+
+    def __init__(self, store_id: int = 1, log_name: str = 'discount_helper', func_name: str = None):
+        super(DiscountHelper, self).__init__(store_id, log_name)
+        self.func_name = func_name if func_name else 'SHOPIFY_DISCOUNT_ID'
+        self._func_id = None
+        self._ns = '$app:discount'
+        self._key = 'config'
+        # Product: 1 << 0
+        # Order: 1 << 1
+        # Shipping: 1 << 2
+        self._code_stamp = 1 << 0
+        # DiscountAutomaticNode, DiscountCodeNode
+        # gid://shopify/DiscountAutomaticNode/
+
+    @property
+    def func_id(self) -> str:
+        if not self._func_id:
+            self._func_id = getenv(self.func_name, None)
+            if self._func_id is None:
+                raise Exception('Environment variable[{}] does not exists!'.format(self.func_name))
+        return self._func_id
+
+    @staticmethod
+    @abstractmethod
+    def get_schema() -> dict:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def format_record_data(cls, data: dict) -> Tuple[dict, Optional[dict]]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def record_to_dict(cls, record: DiscountCode) -> dict:
+        pass
+
+    @abstractmethod
+    def format_meta_data(self, data: dict, owner_id: str = None) -> list:
+        pass
+
+    def format_discount_code_input_data(self, record: DiscountCode) -> dict:
+        input_data = dict(
+            title=record.code_name,
+            code=record.code_name,
+            function_id=self.func_id,
+            applies_once_per_customer=record.one_use_per_customer == 1,
+            combines_with=dict(
+                order_discounts=record.combine_with_order == 1,
+                product_discounts=record.combine_with_product == 1,
+                shipping_discounts=record.combine_with_shipping == 1,
+            ),
+            customer_selection=dict(all=True),
+            starts_at=record.start_date.strftime("%Y-%m-%dT%H:%M:%S"),
+        )
+        if record.limit_usage == 1 and record.maximum_usage is not None:
+            input_data['usage_limit'] = record.maximum_usage
+        if record.end_date:
+            input_data['ends_at'] = record.end_date.strftime("%Y-%m-%dT%H:%M:%S")
+        return input_data
+
+    def format_auto_discount_code_input_data(self, record: DiscountCode) -> dict:
+        input_data = dict(
+            combines_with=dict(
+                order_discounts=record.combine_with_order == 1,
+                product_discounts=record.combine_with_product == 1,
+                shipping_discounts=record.combine_with_shipping == 1,
+            ),
+            title=record.code_name,
+            function_id=self.func_id,
+            starts_at=record.start_date.strftime("%Y-%m-%dT%H:%M:%S"),
+        )
+        if record.end_date:
+            input_data['ends_at'] = record.end_date.strftime("%Y-%m-%dT%H:%M:%S")
+        return input_data
+
+    def create(self, data: dict) -> Tuple[bool, Optional[str], Optional[dict or list]]:
+        record_data, extra_data = self.format_record_data(data)
+        record = DiscountCode.create(
+            store_id=self.store.id,
+            code_stamp=self._code_stamp,
+            **record_data,
+        )
+        if extra_data:
+            record.set_extra(extra_data)
+        db.session.flush()
+
+        if record.code_type == 0:
+            return self._create_code(record, data)
+        return self._create_auto_code(record, data)
+
+    def edit(self, code_id: int) -> Tuple[bool, Optional[str or DiscountCode]]:
+        record = DiscountCode.query.filter_by(store_id=self.store.id, code_id=code_id).first()
+        if not record:
+            return False, f'DiscountNode[{code_id}] does not exists!'
+        return True, record
+
+    def update(self, code_id: int, data: dict) -> Tuple[bool, Optional[str], Optional[dict or list]]:
+        rs, record = self.edit(code_id)
+        if not rs:
+            return False, record, None
+        record_data, extra_data = self.format_record_data(data)
+        record = DiscountCode.create_or_update(
+            dict(store_id=self.store.id, id=record.id),
+            **record_data
+        )
+        if extra_data:
+            record.set_extra(extra_data)
+        db.session.flush()
+        # Check Code Type
+        if record.code_type == 0:
+            return self._update_code(record, data)
+        return self._update_auto_code(record, data)
+
+    def delete(self, code_id: int) -> Tuple[bool, Optional[str], Optional[dict or list]]:
+        rs, record = self.edit(code_id)
+        if not rs:
+            return False, record, None
+        if record.code_type == 0:
+            return self._delete_code(record)
+        return self._delete_auto_code(record)
+
+    def _create_code(self, record: DiscountCode, data: dict) -> Tuple[bool, Optional[str], Optional[dict or list]]:
+        metas = self.format_meta_data(data)
+        op = Operation(shopify_schema.mutation_type, 'CreateCode')
+        input_data = self.format_discount_code_input_data(record)
+        input_data['metafields'] = metas
+        mutation = op.discount_code_app_create(code_app_discount=input_data)
+        mutation.user_errors()
+        mutation.code_app_discount.discount_id()
+        res = self.gql.fetch_data(op)['discountCodeAppCreate']
+        if len(res['userErrors']) > 0:
+            msg = dumps(res['userErrors'])
+            self.logger.error('CodeCreateError: %s', msg)
+            return False, 'Create discount code failed!', res['userErrors']
+        self.logger.debug('CodeCreate: %s', dumps(res))
+        record.code_id = res['codeAppDiscount']['discountId'].split('/')[-1]
+        db.session.commit()
+        return True, None, record.to_dict()
+
+    def _update_code(self, record: DiscountCode, data: dict) -> Tuple[bool, Optional[str], Optional[dict or list]]:
+        op = Operation(shopify_schema.mutation_type, 'UpdateCode')
+        owner_id = 'gid://shopify/DiscountCodeNode/{}'.format(record.code_id)
+        input_data = self.format_discount_code_input_data(record)
+        mutation = op.discount_code_app_update(
+            code_app_discount=input_data,
+            id=owner_id
+        )
+        mutation.user_errors()
+        res = self.gql.fetch_data(op)['discountCodeAppUpdate']
+        if len(res['userErrors']) > 0:
+            msg = dumps(res['userErrors'])
+            self.logger.error('CodeUpdateError: %s', msg)
+            return False, 'Update discount code failed!', res['userErrors']
+        # Update Meta
+        metas = self.format_meta_data(data, owner_id)
+        op = Operation(shopify_schema.mutation_type, 'UpdateMeta')
+        mutation = op.metafields_set(metafields=metas)
+        mutation.user_errors()
+        res = self.gql.fetch_data(op)['metafieldsSet']
+        if len(res['userErrors']) > 0:
+            msg = dumps(res['userErrors'])
+            self.logger.error('UpdateMetaError: %s', msg)
+            return False, 'Update discount code failed!', res['userErrors']
+        db.session.commit()
+        return True, None, record.to_dict()
+
+    def _delete_code(self, record: DiscountCode) -> Tuple[bool, Optional[str], Optional[dict or list]]:
+        op = Operation(shopify_schema.mutation_type, 'DeleteCode')
+        mutation = op.discount_code_delete(id='gid://shopify/DiscountCodeNode/{}'.format(record.code_id))
+        mutation.user_errors()
+        res = self.gql.fetch_data(op)['discountCodeDelete']
+        if len(res['userErrors']) > 0:
+            msg = dumps(res['userErrors'])
+            self.logger.error('DiscountCodeDeleteError: %s', msg)
+            return False, 'Delete discount code failed!', res['userErrors']
+        db.session.delete(record)
+        db.session.commit()
+        return True, None, None
+
+    def _create_auto_code(self, record: DiscountCode, data: dict) -> Tuple[
+        bool, Optional[str], Optional[dict or list]]:
+        metas = self.format_meta_data(data)
+        op = Operation(shopify_schema.mutation_type, 'CreateAutoCode')
+        input_data = self.format_auto_discount_code_input_data(record)
+        input_data['metafields'] = metas
+        mutation = op.discount_automatic_app_create(automatic_app_discount=input_data)
+        mutation.user_errors()
+        mutation.automatic_app_discount.discount_id()
+        res = self.gql.fetch_data(op)['discountAutomaticAppCreate']
+        if len(res['userErrors']) > 0:
+            msg = dumps(res['userErrors'])
+            self.logger.error(msg)
+            return False, 'Create automatic discount code failed!', res['userErrors']
+        self.logger.debug('AutomaticCodeCreate: %s', dumps(res))
+        record.code_id = res['automaticAppDiscount']['discountId'].split('/')[-1]
+        db.session.commit()
+        return True, None, self.record_to_dict(record)
+
+    def _update_auto_code(self, record: DiscountCode, data: dict) -> Tuple[
+        bool, Optional[str], Optional[dict or list]]:
+        op = Operation(shopify_schema.mutation_type, 'UpdateAutoCode')
+        owner_id = 'gid://shopify/DiscountAutomaticNode/{}'.format(record.code_id)
+        input_data = self.format_auto_discount_code_input_data(record)
+        mutation = op.discount_automatic_app_update(
+            automatic_app_discount=input_data,
+            id=owner_id
+        )
+        mutation.user_errors()
+        res = self.gql.fetch_data(op)['discountAutomaticAppUpdate']
+        if len(res['userErrors']) > 0:
+            msg = dumps(res['userErrors'])
+            self.logger.error('AutomaticCodeUpdateError: %s', msg)
+            return False, 'Update automatic discount code failed!', res['userErrors']
+        # Update Meta
+        metas = self.format_meta_data(data, owner_id)
+        op = Operation(shopify_schema.mutation_type, 'UpdateMeta')
+        mutation = op.metafields_set(metafields=metas)
+        mutation.user_errors()
+        res = self.gql.fetch_data(op)['metafieldsSet']
+        if len(res['userErrors']) > 0:
+            msg = dumps(res['userErrors'])
+            self.logger.error('UpdateMetaError: %s', msg)
+            return False, 'Update automatic discount code failed!', res['userErrors']
+        db.session.commit()
+        return True, None, self.record_to_dict(record)
+
+    def _delete_auto_code(self, record: DiscountCode) -> Tuple[bool, Optional[str], Optional[dict or list]]:
+        op = Operation(shopify_schema.mutation_type, 'DeleteCode')
+        mutation = op.discount_automatic_delete(id='gid://shopify/DiscountAutomaticNode/{}'.format(record.code_id))
+        mutation.user_errors()
+        res = self.gql.fetch_data(op)['discountAutomaticDelete']
+        if len(res['userErrors']) > 0:
+            msg = dumps(res['userErrors'])
+            self.logger.error('AutomaticCodeDeleteError: %s', msg)
+            return False, 'Delete automatic discount code failed!', res['userErrors']
+        db.session.delete(record)
+        db.session.commit()
+        return True, None, None
